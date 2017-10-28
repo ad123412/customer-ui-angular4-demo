0212c1f3d79ee04cdf59d47f97d873afafb00bbe
ad123412 <ad123412@gmail.com>
2017-10-28 17:53:35 +0530

    First commit

--- src/app/services/customer-service.service.ts
+++ src/app/services/customer-service.service.ts
@@ -13,7 +13,8 @@ export class CustomerServiceService {
     // %2B436641234567
     // http://localhost:3000/customer
     // 'http://localhost:30011/customer/contact/' + dn
-    return this._http.get('http://localhost:30011/customer/contact/' + dn);
+    //http://www.tyk-portal-test.com/digitalplatformcustomerdetails/rest-experience/customer/details/%2B436641234567
+    return this._http.get('http://www.tyk-portal-test.com/digitalplatformcustomerdetails/rest-experience/customer/details/' + dn);
       //.map(response => response.json());
   }
 }
